22e89d97b57460fb74e7d90c57b2ede6463a6ec1
Libby Henry <libbh52@gmail.com>
2019-06-25 21:45:02 +0100

    Premlinary code for Bug and Comment model created

--- bugs/models.py
+++ bugs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Bug(models.Model):
+    """
+    A single bug report
+    """
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    details = models.TextField(max_length=300)
+    published_date = models.DateField(null=True, blank=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=300)
+    published_date = models.DateField(null=True, blank=True, default=timezone.now)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.comment
