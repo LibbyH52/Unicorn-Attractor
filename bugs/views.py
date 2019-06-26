3a61b5953bd8a2cc5f5aa31a7872cf826ba4b568
Libby Henry <libbyh52@gmail.com>
2019-06-26 17:20:35 +0100

    Created view for displaying 'all bugs', added bugs url to main project urls and created url to all bugs for bugs app

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Bug, Comment
 
-# Create your views here.
+def show_bugs(request):
+    """
+    This view will return a list of all
+    bugs in date order and display them on the
+    'allbugs' template.
+    """
+    bugs = Bug.objects.order_by('-posted_on').all() 
+    return render(request, 'bugs/allbugs.html', {'bugs':bugs})
+
+
