7a51372afb11d36676cc1087e156a47491eb210b
Libby Henry <libbyh52@gmail.com>
2019-06-27 12:11:35 +0100

    Created view, url and template for single bug view and added link and number of views to allbugs

--- bugs/views.py
+++ bugs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Bug, Comment
 
 def show_bugs(request):
@@ -10,4 +10,14 @@ def show_bugs(request):
     bugs = Bug.objects.order_by('-posted_on').all() 
     return render(request, 'bugs/allbugs.html', {'bugs':bugs})
 
+def bug_description(request, pk):
+    """
+    This view allows a user to click on a particular
+    bug to find out more details about it and add a
+    comment
+    """
+    bug = get_object_or_404(Bug, pk=pk)
+    bug.views += 1
+    bug.save()
+    return render(request, "bugs/bugdescription.html", {"bug":bug})
 
