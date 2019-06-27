3091d9b202cda4206ddeac8f43d7bf4a82671f97
Libby Henry <libbh52@gmail.com>
2019-06-27 19:08:33 +0100

    Updated models to allow users to author a bug report and comment on bugs

--- bugs/models.py
+++ bugs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Bug(models.Model):
     """
@@ -17,8 +19,8 @@ class Bug(models.Model):
         ]
     fix_status = models.CharField(max_length=10, choices=fix_status_choices, default='To Do')
     posted_on = models.DateField(null=True, blank=True, default=timezone.now)
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = models.CharField(max_length=20)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.CharField(max_length=100, null=False, blank=False)
     views = models.IntegerField(default=0)
 
     def __str__(self):
@@ -26,9 +28,9 @@ class Bug(models.Model):
 
 class Comment(models.Model):
     comment = models.TextField(max_length=300)
-    posted_on = models.DateField(null=True, blank=True, default=timezone.now)
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = models.CharField(max_length=20)
+    created_on = models.DateField(null=True, blank=True, default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.CharField(max_length=100, null=False, blank=False)
     bug = models.ForeignKey(Bug, on_delete=models.CASCADE)
 
     def __str__(self):
