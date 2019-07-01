17fc758e98be2659f207e38d0e83402f9c424277
Libby Henry <libbh52@gmail.com>
2019-07-01 19:11:32 +0100

    Created registration view and templates and added flash messages to base templates

--- users/views.py
+++ users/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 
 def registration(request):
     """
-    Renders a page with a registration form,
+    Renders a page with a registration form
     so users can sign up for the site
     """
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            messages.success(request, f"Account successfully created for {username}")
+            return redirect("show_bugs")
+    
+    else:
+        form = UserRegistrationForm()
     return render(request, 'users/registration.html', {'form':form})
