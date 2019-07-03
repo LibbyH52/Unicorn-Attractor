eb0d73bd2f8f88ca36f59672515fd932fa19eb62
Libby Henry <libbh52@gmail.com>
2019-07-03 13:46:52 +0100

    Created url and view for logout

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import registration, login
+from .views import registration, login, logout
 
 
 urlpatterns = [
     path('registration/', registration, name="registration"),
     path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
 ]
