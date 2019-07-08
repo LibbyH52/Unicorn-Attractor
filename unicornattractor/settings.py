ef74148ddf1da2682ab4a20545fe767a0bd7765f
Libby Henry <libbh52@gmail.com>
2019-07-08 20:56:47 +0100

    Fixed indentation error on 'else' statement

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -90,8 +90,8 @@ if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
     } 
-    else:
-        DATABASES = {
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
