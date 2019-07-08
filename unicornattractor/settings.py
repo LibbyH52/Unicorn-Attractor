f32afa2f6c59736086f9c8116f5e4a353d3379e5
Libby Henry <libbh52@gmail.com>
2019-07-08 21:01:24 +0100

    Added local host and heroku app name to 'ALLOWED_HOSTS'

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unicorn-attractor-milestone-4.heroku.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'unicorn-attractor-milestone-4.herokuapp.com']
 
 
 # Application definition
