2b43da3dee6fa3cdaa3c142f605d0993868a22c1
Libby Henry <libbyh52@gmail.com>
2019-07-08 20:52:39 +0100

    Removed white space between 'else' and ':'

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 """
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        'default' :
-            dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    } else :
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    } 
+    else:
+        DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }   
 
