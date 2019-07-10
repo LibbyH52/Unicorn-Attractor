ff8cc3f58f8da325acac888dc9226b7b31323502
Libby Henry <libbyh52@gmail.com>
2019-07-10 21:24:41 +0100

    Created profile view, url and template and styled and added image to profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login, logout, registration
+from .views import login, logout, registration, user_profile
 
 
 urlpatterns = [
     path('', registration, name="registration"),
-    path('accounts/login/', login, name="login"),
+    path('login/', login, name="login"),
     path('logout/', logout, name="logout"),
+    path('profile/', user_profile, name="profile"),
 ]
