3a8ab64750b055d7dff6ba313b030515ef792cd7
Libby Henry <libbh52@gmail.com>
2019-07-11 14:43:22 +0100

    Separated 'add_or_edit_bug' into two separate functions, created an 'edit_comment' function and updated link in html file. Added margin to 'btn-comment' button

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import show_bugs, bug_description, add_or_edit_bug, add_comment
+from .views import show_bugs, bug_description, add_bug, edit_bug, add_comment, edit_comment
 
 urlpatterns = [
     path('show_bugs/', show_bugs, name="show_bugs"),
     path('<int:pk>/', bug_description, name="bug_description"),
-    path('add_bug/', add_or_edit_bug, name="add_bug"),
-    path('<int:pk>/edit/', add_or_edit_bug, name="edit_bug"),
+    path('add_bug/', add_bug, name="add_bug"),
+    path('<int:pk>/edit_bug/', edit_bug, name="edit_bug"),
     path('<int:pk>/add_comment/', add_comment, name="add_comment"),
+    path('<int:pk>/edit_comment/', edit_comment, name="edit_comment"),
 ]
