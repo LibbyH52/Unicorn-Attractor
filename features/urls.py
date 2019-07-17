025070bfd84698ec5224daf4d2be57966fdf706d
Libby Henry <libbh52@gmail.com>
2019-07-17 20:58:22 +0100

    Created form and template to allow users to request a feature. Create view and url for same. Linked to form in allfeatures template

--- features/urls.py
+++ features/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features
+from .views import get_features, new_feature
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
+    path('new_feature/', new_feature, name="new_feature"),
 ]
