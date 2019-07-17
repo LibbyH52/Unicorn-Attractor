f51c9c63b1bb6ee2c8d1bb2aacb27b5f02d9771e
Libby Henry <libbyh52@gmail.com>
2019-07-17 20:58:22 +0100

    Created form and template to allow users to request a feature. Create view and url for same. Linked to form in allfeatures template

--- features/views.py
+++ features/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect,  get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Feature
+from .forms import AddFeatureForm
 
 
+@login_required()
 def get_features(request):
     """
     A view that returns a list
@@ -12,4 +17,20 @@ def get_features(request):
     features = Feature.objects.order_by('-posted_on').all()
     return render(request, 'features/allfeatures.html', {'features': features})
 
-  
+
+@login_required()
+def new_feature(request):
+    """
+    A view that renders a form to allow
+    a user to request a feature.
+    """
+    if request.method == "POST":
+        form = AddFeatureForm(request.POST)
+        if form.is_valid():
+            feature = form.save(commit=False)
+            feature.requested_by = request.user
+            feature.save()
+            return redirect('get_features')
+    else:
+        form = AddFeatureForm()
+    return render(request, 'features/addfeature.html', {'form': form})
