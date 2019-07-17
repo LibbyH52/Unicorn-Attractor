ef0407ac3d443067d72aad13299db0772615964a
Libby Henry <libbyh52@gmail.com>
2019-07-17 16:06:48 +0100

    Removed number of 'views' from model, as not relevant for Features. Added PHASE_OF_DEVELOPMENT to models for visual clues for user. Removed 'feature_details' from views and urls as displaying full feature on one template

--- features/views.py
+++ features/views.py
@@ -12,17 +12,4 @@ def get_features(request):
     features = Feature.objects.order_by('-posted_on').all()
     return render(request, 'features/allfeatures.html', {'features': features})
 
-
-def feature_details(request, pk):
-    """
-    A view that returns details of
-    a single feature based on the primary key
-    and renders it to the 'featuredetails.html'
-    template. If the feature is not found a 404
-    error will be returned.
-    """
-    feature = get_object_or_404(Feature, pk=pk)
-    views += 1
-    upvote += 1
-    feature.save()
-    return render(request, 'featuredetails.html', {'feature': feature})
+  
