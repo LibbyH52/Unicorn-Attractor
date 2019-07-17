f3c9ccdffa95daebb7bae16d8bd945c2c083f2a0
Libby Henry <libbh52@gmail.com>
2019-07-17 16:12:32 +0100

    Forgot file in last push

--- features/urls.py
+++ features/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features, feature_details
+from .views import get_features
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
-    path('feature_details/', feature_details, name="feature_details"),
 ]
