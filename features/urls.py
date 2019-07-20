f92966e7db3c49c5f7cd7eb2e6c0a4113b30b99e
Libby Henry <libbh52@gmail.com>
2019-07-20 11:29:27 +0100

    Removed 'upvote_feature' as upvotes will be linked to cart app

--- features/urls.py
+++ features/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features, new_feature, upvote_feature
+from .views import get_features, new_feature
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
     path('new_feature/', new_feature, name="new_feature"),
-    path('<int:pk>/upvote_feature/', upvote_feature, name="upvote_feature"),
 ]
