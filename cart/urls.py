d615790dce6ea8ca35f875c74152c31db182e75e
Libby Henry <libbh52@gmail.com>
2019-07-20 18:06:58 +0100

    Created cart app, added cart.contexts to context processors in settings, added cart url to project urls file and created form in features page for adding feature to cart

---
+++ cart/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import view_cart, add_to_cart
+
+urlpatterns = [
+    path('view_cart/', view_cart, name="view_cart"),
+    path('<int:id>/add_to_cart/', add_to_cart, name="add_to_cart"),
+]
