06c64c0f5dbbec7caf858c3e9acb6cf2d08f5ee3
Libby Henry <libbh52@gmail.com>
2019-07-31 22:59:28 +0100

    Added vote price to feature model, ran migrations new fields and updated realted files

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,14 +9,12 @@ def cart_contents(request):
     cart = request.session.get('cart', {})
     cart_items = []
     
-    price = 10
     total = 0
     feature_count = 0
  
     for id, quantity in cart.items():
         feature = get_object_or_404(Feature, pk=id)
-        price = price * quantity
-        total += price
+        total += quantity * feature.vote_price
         feature_count += quantity
-        cart_items.append({'id': id, 'quantity': quantity, 'feature': feature, 'price': price})
+        cart_items.append({'id': id, 'quantity': quantity, 'feature': feature, 'feature.vote_price': feature.vote_price})
     return {'cart_items': cart_items, 'total': total, 'feature_count': feature_count}
