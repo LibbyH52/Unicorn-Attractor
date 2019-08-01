e9e8186de557d5f6b36a7bb1e78bdcce3b7ec14a
Libby Henry <libbyh52@gmail.com>
2019-08-01 15:16:29 +0100

    Fixed typo for 'county' and 'postcode' .

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,14 +6,14 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036 )]
 
-    credit_card_number = form.CharField(label='Credit Card Number', required=False)
-    cvv = form.CharField(label='Security Code(CVV)')
-    expiry_year = models.ChoiceField(label='year', choices=YEAR_CHOICES, required=False)
-    expiry_month = models.ChoiceField(label='month', choices=MONTH_CHOICES, required=False)
+    credit_card_number = forms.CharField(label='Credit Card Number', required=False)
+    cvv = forms.CharField(label='Security Code(CVV)')
+    expiry_year = forms.ChoiceField(label='year', choices=YEAR_CHOICES, required=False)
+    expiry_month = forms.ChoiceField(label='month', choices=MONTH_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'phone_number', 'country', 'post_code', 'town_or_city', 
+        fields = ('full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 
                     'street_address1', 'street_address2', 'county')
