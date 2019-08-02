afa501311e5e7472e7f58054d39cc229b16a5c1c
Libby Henry <libbh52@gmail.com>
2019-08-02 22:04:57 +0100

    Removed unnecessary information from order model

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,5 +15,4 @@ class MakePaymentForm(forms.Form):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 
-                    'street_address1', 'street_address2', 'county')
+        fields = ('full_name',)
--- checkout/models.py
+++ checkout/models.py
@@ -5,13 +5,6 @@ from features.models import Feature
 
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
-    county = models.CharField(max_length=40, blank=False, default="")
-    country = CountryField(blank_label='(select country)')
     date = models.DateField()
 
     def __str__(self):
