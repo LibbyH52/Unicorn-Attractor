b43b69eb7d1d56c7dff1cd3445e0c95e24fc7e1d
Libby Henry <libbyh52@gmail.com>
2019-08-03 11:57:59 +0100

    Reinstated country field in model to be more similar to Stripe payment form. Ran migratons and updated form

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,4 +15,4 @@ class MakePaymentForm(forms.Form):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name',)
+        fields = ('full_name', 'country')
