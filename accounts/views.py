f8e40526d1161019449f7fc213c504c71fe4fcdb
Libby Henry <libbyh52@gmail.com>
2019-08-07 18:30:39 +0100

    Added bugs and features created by users to their profile page. Updated styles on pages also

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserLoginForm
+from bugs.models import Bug
+from features.models import Feature
 
 
 def registration(request):
@@ -33,7 +35,9 @@ def user_profile(request):
     Profile page for the logged in user.
     """
     user = User.objects.get(username=request.user.username)
-    return render(request, "accounts/profile.html", {"profile": user})
+    bugs = Bug.objects.filter(author=user)
+    features = Feature.objects.filter(requested_by=user)
+    return render(request, "accounts/profile.html", {"profile": user, 'bugs': bugs, 'features': features})
 
 
 def login(request):
