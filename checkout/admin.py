c9f39bf0d793391d580012633daf98f4d038e10b
Libby Henry <libbh52@gmail.com>
2019-08-07 16:00:40 +0100

    Deleted Order and OrderLineItem as not relevant for upvoting feature. Removed references to OrderForm from views.y and checkout.py and deleted from admin.py. Updated views.py and checkout template. Ran migrations for empty model.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
 
-
-class OrderLineAdminInline(admin.TabularInline):
-    model = OrderLineItem
-
-class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineAdminInline, )
-
-admin.site.register(Order, OrderAdmin)
+# Register your models here.
--- checkout/models.py
+++ checkout/models.py
@@ -1,20 +1,3 @@
 from django.db import models
-from django_countries.fields import CountryField
-from features.models import Feature
 
-
-class Order(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
-    country = CountryField(blank_label='(select country)', default="")
-    date = models.DateField()
-
-    def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
-
-class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
-    feature = models.ForeignKey(Feature, null=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(blank=False)
-
-    def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.feature.name, self.feature.vote_price)
+# Create your models here.
