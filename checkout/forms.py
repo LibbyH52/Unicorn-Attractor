a1589dbd8814dc31e85482498324dd2ab9c013ec
Libby Henry <libbyh52@gmail.com>
2019-08-07 16:00:40 +0100

    Deleted Order and OrderLineItem as not relevant for upvoting feature. Removed references to OrderForm from views.y and checkout.py and deleted from admin.py. Updated views.py and checkout template. Ran migrations for empty model.

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,18 +1,15 @@
 from django import forms
-from .models import Order
+from django_countries.fields import CountryField
 
 
 class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036 )]
-
+    
+    full_name = forms.CharField(label="Name")
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
-    cvv = forms.CharField(label='Security Code(CVV)')
+    cvv = forms.CharField(label='Security Code(CVV)', required=False)
     expiry_year = forms.ChoiceField(label='year', choices=YEAR_CHOICES, required=False)
     expiry_month = forms.ChoiceField(label='month', choices=MONTH_CHOICES, required=False)
+    country = CountryField().formfield()
     stripe_id = forms.CharField(widget=forms.HiddenInput)
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ('full_name', 'country')
