4f517801633d25cfa21da9d068f4a9e59dd69f21
Libby Henry <libbh52@gmail.com>
2019-08-14 21:37:54 +0100

    Replaced double quotes with singe for consistency

--- bugs/test_forms.py
+++ bugs/test_forms.py
@@ -4,13 +4,13 @@ from bugs.forms import AddBugForm, AddCommentForm
 
 class TestAddBugForm(TestCase):
     def test_can_create_bug_with_missing_data(self):
-        form = AddBugForm({"title": "A New Bug", "details": ""})
+        form = AddBugForm({'title': 'A New Bug', 'details': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['details'], [u'This field is required.'])
 
 
 class TestAddCommentForm(TestCase):
     def test_can_create_bug_with_missing_data(self):
-        form = AddCommentForm({"comment": " "})
+        form = AddCommentForm({'comment': ' '})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['comment'], [u'This field is required.'])
