9d446bb1fface88b3506319e2463c920cd0dcabf
Libby Henry <libbyh52@gmail.com>
2019-08-18 21:45:49 +0100

    Created add_comment view and added new url

--- features/urls.py
+++ features/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features, feature_description, new_feature, edit_feature
+from .views import get_features, feature_description, new_feature, edit_feature, add_comment
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
     path('<int:pk>', feature_description, name="feature_description"),
     path('new_feature/', new_feature, name="new_feature"),
-    path('<int:pk>edit_feature/', edit_feature, name="edit_feature")
+    path('<int:pk>edit_feature/', edit_feature, name="edit_feature"),
+    path('<int:pk>/add_comment/', add_comment, name="add_comment"),
 ]
