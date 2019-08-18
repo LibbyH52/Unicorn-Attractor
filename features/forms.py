48c0553841ca185cfb30b90961eedc45e23af1c6
Libby Henry <libbh52@gmail.com>
2019-08-18 21:37:24 +0100

    Created new model and form for commenting on feature and registered it on admin.py

--- features/forms.py
+++ features/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Feature
+from .models import Feature, Comment
 
 
 class AddFeatureForm(forms.ModelForm):
     class Meta:
         model = Feature
         fields = ('name', 'details')
+
+class AddCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
