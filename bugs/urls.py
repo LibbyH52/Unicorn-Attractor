0c434f1e70b85459b8d779baae189ff4f50b5add
Libby Henry <libbyh52@gmail.com>
2019-08-19 15:38:13 +0100

    Updated views and urls for comments to contain 'bug' to differentiate between feature comments. Amended urls in template to reflect same.

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import show_bugs, bug_description, add_bug, edit_bug, add_comment, edit_comment, delete_comment
+from .views import show_bugs, bug_description, add_bug, edit_bug, add_bug_comment, edit_bug_comment, delete_bug_comment
 
 urlpatterns = [
     path('show_bugs/', show_bugs, name="show_bugs"),
     path('<int:pk>/', bug_description, name="bug_description"),
     path('add_bug/', add_bug, name="add_bug"),
     path('<int:pk>/edit_bug/', edit_bug, name="edit_bug"),
-    path('<int:pk>/add_comment/', add_comment, name="add_comment"),
-    path('<int:pk>/edit_comment/', edit_comment, name="edit_comment"),
-    path('<int:pk>/delete_comment/', delete_comment, name="delete_comment"),
+    path('<int:pk>/add_bug_comment/', add_bug_comment, name="add_bug_comment"),
+    path('<int:pk>/edit_bug_comment/', edit_bug_comment, name="edit_bug_comment"),
+    path('<int:pk>/delete_bug_comment/', delete_comment, name="delete_bug_comment"),
 ]
