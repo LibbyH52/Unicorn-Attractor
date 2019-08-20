5888f5419d406196edf68baaad4f279d4c2bd1c0
Libby Henry <libbyh52@gmail.com>
2019-08-20 19:42:55 +0100

    Added missing forward slashes (/) to relevant urls

--- features/urls.py
+++ features/urls.py
@@ -4,10 +4,10 @@ from .views import get_features, feature_description, new_feature, edit_feature,
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
-    path('<int:pk>', feature_description, name="feature_description"),
+    path('<int:pk>/', feature_description, name="feature_description"),
     path('new_feature/', new_feature, name="new_feature"),
-    path('<int:pk>edit_feature/', edit_feature, name="edit_feature"),
+    path('<int:pk>/edit_feature/', edit_feature, name="edit_feature"),
     path('<int:pk>/add_feature_comment/', add_feature_comment, name="add_feature_comment"),
-    path('<int:pk>edit_feature_comment/', edit_feature_comment, name="edit_feature_comment"),
-    path('<int:pk>delete_feature_comment/', delete_feature_comment, name="delete_feature_comment"),
+    path('<int:pk>/edit_feature_comment/', edit_feature_comment, name="edit_feature_comment"),
+    path('<int:pk>/delete_feature_comment/', delete_feature_comment, name="delete_feature_comment"),
 ]
