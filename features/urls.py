35cb702dafecd8882d4d90ae1ad393832fb4a3ae
Libby Henry <libbyh52@gmail.com>
2019-08-20 18:43:12 +0100

    Added 'feature' to view names for add, editing and deleting feature related comments. Renamed template addfeaturecomment.html

--- features/urls.py
+++ features/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features, feature_description, new_feature, edit_feature, add_comment, edit_comment, delete_comment
+from .views import get_features, feature_description, new_feature, edit_feature, add_feature_comment, edit_feature_comment, delete_feature_comment
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
     path('<int:pk>', feature_description, name="feature_description"),
     path('new_feature/', new_feature, name="new_feature"),
     path('<int:pk>edit_feature/', edit_feature, name="edit_feature"),
-    path('<int:pk>/add_comment/', add_comment, name="add_comment"),
-    path('<int:pk>edit_comment/', edit_comment, name="edit_comment"),
-    path('<int:pk>delete_comment/', delete_comment, name="delete_comment"),
+    path('<int:pk>/add_feature_comment/', add_feature_comment, name="add_feature_comment"),
+    path('<int:pk>edit_feature_comment/', edit_feature_comment, name="edit_feature_comment"),
+    path('<int:pk>delete_feature_comment/', delete_feature_comment, name="delete_feature_comment"),
 ]
