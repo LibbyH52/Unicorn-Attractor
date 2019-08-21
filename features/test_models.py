df1450bce5ad93bdab070354af5eb38b48bd53e5
Libby Henry <libbh52@gmail.com>
2019-08-21 11:03:11 +0100

    Fixed merge conflict

--- features/test_models.py
+++ features/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Feature
+from .models import Feature, Comment
 
 
 class FeatureModelTest(TestCase):
@@ -10,3 +10,12 @@ class FeatureModelTest(TestCase):
     def test_str(self):
         feature = Feature(name='New Feature')
         self.assertEqual(str(feature), feature.name)
+
+class CommentModelTest(TestCase):
+    """
+    Test to be run against
+    the Comment model
+    """
+    def test_str(self):
+        comment = Comment(comment='I want this too')
+        self.assertEqual(str(comment), comment.comment)
