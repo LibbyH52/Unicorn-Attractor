8d3b970b3bd4da54f05cdc1e6ef7fcde8e17bfd1
Libby Henry <libbh52@gmail.com>
2019-08-22 15:56:09 +0100

    Entered placeholder and widget for credit card number

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,9 +7,9 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036 )]
     
     full_name = forms.CharField(label="Name")
-    credit_card_number = forms.CharField(label='Credit Card Number', required=False)
-    cvv = forms.CharField(label='Security Code(CVV)', required=False)
+    credit_card_number = forms.CharField(label='Credit Card Number', required=False, widget=forms.TextInput(attrs={'pattern': '[0-9]{4} *[0-9]{4} *[0-9]{4} *[0-9]{4}', 'placeholder': '4242 4242 4242 4242'}))
+    cvv = forms.CharField(label='Security Code(CVV)', required=False, widget=forms.TextInput(attrs={'pattern': '[0-9]{3}'}))
     expiry_year = forms.ChoiceField(label='year', choices=YEAR_CHOICES, required=False)
     expiry_month = forms.ChoiceField(label='month', choices=MONTH_CHOICES, required=False)
-    country = CountryField().formfield()
+    country = CountryField(default='IE').formfield()
     stripe_id = forms.CharField(widget=forms.HiddenInput)
