c09a74a716697e5a302fd563096ed706957ab4b6
Libby Henry <libbyh52@gmail.com>
2019-08-27 14:15:04 +0100

    PEP8 compliant and comments added

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,12 +1,22 @@
 from django import forms
 from .models import Bug, Comment
 
+
 class AddBugForm(forms.ModelForm):
+    """
+    Creates a form to allow a user to
+    add a bug report
+    """
     class Meta:
         model = Bug
         fields = ('title', 'details')
 
+
 class AddBugCommentForm(forms.ModelForm):
+    """
+    Creates a form to allow a user to
+    comment on a bug report
+    """
     class Meta:
         model = Comment
         fields = ('comment',)
