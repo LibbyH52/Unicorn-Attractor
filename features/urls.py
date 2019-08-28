1d6473d9b3ab4360be38c6ae1190f2f58a7eda70
Libby Henry <libbyh52@gmail.com>
2019-08-28 19:10:48 +0100

    Forgotten in previous add

--- features/urls.py
+++ features/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_features, feature_description, new_feature, edit_feature, add_feature_comment, edit_feature_comment, delete_feature_comment
+from .views import get_features, feature_description, new_feature, edit_feature, delete_feature, add_feature_comment, edit_feature_comment, delete_feature_comment
 
 urlpatterns = [
     path('get_features/', get_features, name="get_features"),
     path('<int:pk>/', feature_description, name="feature_description"),
     path('new_feature/', new_feature, name="new_feature"),
     path('<int:pk>/edit_feature/', edit_feature, name="edit_feature"),
+    path('<int:pk>/delete_feature/', delete_feature, name="delete_feature"),
     path('<int:pk>/add_feature_comment/', add_feature_comment, name="add_feature_comment"),
     path('<int:pk>/edit_feature_comment/', edit_feature_comment, name="edit_feature_comment"),
     path('<int:pk>/delete_feature_comment/', delete_feature_comment, name="delete_feature_comment"),
