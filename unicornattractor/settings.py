9ed917c3b42681914f62f4255958241096aad6aa
Libby Henry <libbh52@gmail.com>
2019-08-29 22:11:51 +0100

    Removed local host for production

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'unicorn-attractor-milestone-4.herokuapp.com']
+ALLOWED_HOSTS = ['unicorn-attractor-milestone-4.herokuapp.com']
 
 
 # Application definition
