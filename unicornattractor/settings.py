de9c6d2d3ef51599d37c2a5934088f77ee50032b
Libby Henry <libbh52@gmail.com>
2019-08-30 21:13:39 +0100

    Stying for cart and checkout

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -153,8 +153,8 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
