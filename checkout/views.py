1e369d11126f634c6cc53c472e158aaedc5dbb39
Libby Henry <libbyh52@gmail.com>
2019-08-31 16:09:31 +0100

    PEP8 compliant

--- checkout/views.py
+++ checkout/views.py
@@ -13,36 +13,39 @@ stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def checkout(request):
-	upvotes = {}
-	if request.method == "POST":
-		payment_form = MakePaymentForm(request.POST)
-		if payment_form.is_valid():
-			cart = request.session.get('cart', {})
-			total = 0
-			for id, quantity in cart.items():
-				feature = get_object_or_404(Feature, pk=id)
-				total += quantity * feature.vote_price
-				feature.upvote += quantity
-				feature.save()
-			try:
-				charge = stripe.Charge.create(
-				amount = int(total * 100),
-				currency = "EUR",
-				description = request.user.email,
-				card = payment_form.cleaned_data['stripe_id']
-				)
-			except stripe.error.CardError:
-				messages.error(request, "Your card was declined!")
-			if charge.paid:
-				messages.success(request, "You have successfully paid!")
-				request.session['cart'] = {}
-				return redirect(reverse('get_features'))
-			else:
-				messages.error(request, "Unable to take payment!")
-		else:
-			print(payment_form.errors)
-			messages.error(request, "We were unable to take payment with that card!")
-	else:
-		payment_form = MakePaymentForm()
-	return render(request, 'checkout/checkout.html', {'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
-
+    upvotes = {}
+    if request.method == "POST":
+        payment_form = MakePaymentForm(request.POST)
+        if payment_form.is_valid():
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                feature = get_object_or_404(Feature, pk=id)
+                total += quantity * feature.vote_price
+                feature.upvote += quantity
+                feature.save()
+            try:
+                charge = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id'])
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            if charge.paid:
+                messages.success(request, "You have successfully paid!")
+                request.session['cart'] = {}
+                return redirect(reverse('get_features'))
+            else:
+                messages.error(request, "Unable to take payment!")
+        else:
+            print(payment_form.errors)
+            messages.error(request,
+                           "We were unable to take payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+    return render(request, 'checkout/checkout.html',
+                  {'payment_form': payment_form,
+                   'publishable': settings.STRIPE_PUBLISHABLE
+                   }
+                  )
